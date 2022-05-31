c9359c85ea2480196138fe91d8d5acf6792aa51f
greggrivas <grivas.grigoris@gmail.com>
2022-05-31 16:13:45 +0300

    version 1.0

--- core/converter.py
+++ core/converter.py
@@ -1,3 +1,34 @@
 import os
 
-print(test)
+class Converter:
+    """
+    The Converter class which will perform the necessary conversions
+    """
+    def __int__(self):
+        """
+        the constructor of the Converter class for the initialization of the required variables
+        """
+        self.hours = 0
+        self.mins = 0
+        self.secs = 0
+        self.degs = 0.0
+        self.temp = 0
+        self.temp2 = 0
+
+    @staticmethod
+    def hours_to_degs(hours, mins, secs):
+        degs = float((hours + mins / 60 + secs / 3600) * 15)
+        return degs
+
+    @staticmethod
+    def degs_to_hours(degs: float):
+        hours = int(degs / 15)
+        temp = degs / 15
+        mins = int(60 * (temp - hours))
+        temp2 = 60 * (temp - hours)
+        secs = int(round(60 * (temp2 - int(mins)),1))
+        return hours, mins, secs
+
+# test = Converter()
+# print(test.hours_to_degs(3,3,3))
+
---
+++ core/main.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import logging
+
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from converter import Converter
+
+
+class UI_Converter(QtWidgets.QApplication):
+
+    def __init__(self, parent=None):
+        super(UI_Converter, self).__init__(parent)
+        self.logger = logging.getLogger(__name__)  # Create the logger for the file
+        self.h = ''
+        # Load resource file
+        resource_object = QtCore.QResource()
+        resource_file = resource_object.registerResource(os.path.abspath("resources.rcc"))
+        if resource_file.bit_length() == 0:
+            self.logger.error("Resources file could not be loaded. Program is exiting.")
+
+        self.main_window = QtWidgets.QMainWindow() # Create the main GUI window or any other windows
+        self.main_widget = self.ui_loader(':/ui/ConverterUI', self.main_window) # Loading the respective UI files
+        self.setup_ui()
+
+    def setup_ui(self):
+        # the input for the first box
+        self.main_window.hour_line_in.text()
+        self.main_window.mins_line_in.text()
+        self.main_window.secs_line_in.text()
+        self.main_window.calc_degs.clicked.connect(self.clicked1)
+        # the input for the second box
+        self.main_window.degs_line_in.text()
+        self.main_window.calc_hours.clicked.connect(self.clicked2)
+
+
+    def clicked1(self):
+        h = Converter.hours_to_degs(int(self.main_window.hour_line_in.text()),
+                                    int(self.main_window.mins_line_in.text()),
+                                    int(self.main_window.secs_line_in.text()))
+        self.main_window.degs_line_out.setText(str(h))
+
+    def clicked2(self):
+        h2 = Converter.degs_to_hours(int(self.main_window.degs_line_in.text()))
+        self.main_window.hour_line_out.setText(str(h2[0]))
+        self.main_window.mins_line_out.setText(str(h2[1]))
+        self.main_window.secs_line_out.setText(str(h2[2]))
+
+    def show_app(self):
+        self.main_widget.show()
+
+    @staticmethod
+    def ui_loader(ui_resource, base=None):
+        ui_file = QtCore.QFile(ui_resource)
+        ui_file.open(QtCore.QFile.ReadOnly)
+        try:
+            parsed_ui = uic.loadUi(ui_file, base)
+            return parsed_ui
+        finally:
+            ui_file.close()
+
+
+app = UI_Converter(sys.argv)
+app.show_app()
+sys.exit(app.exec())
+
+# /home/ggrivas/PycharmProject/Converter/core/resources.rcc
